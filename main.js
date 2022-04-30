05965d69691e4cb61181e250e9e24d972a80d109
shukurenai09 <78141688+jashpanchal@users.noreply.github.com>
2022-04-30 11:01:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
+noseX = 0;
+noseY = 0;
 function setup(){
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(400,300);
     canvas.center();
     
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function photo(){
     save("tumaraphotu.png");
+}
+function modelLoaded(){
+    console.log('poseNet is initialized!');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        
+        noseX = results[0].pose.nose.x - 15;
+        noseY = results[0].pose.nose.y - 15;
+
+        console.log("nose x = "+noseX);
+        console.log("nose y = "+noseY);
+    
+      }
+}
+function draw(){
+    image(video,0,0,400,300);
 }
